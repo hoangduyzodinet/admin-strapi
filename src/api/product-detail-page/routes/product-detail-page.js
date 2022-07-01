ea22018f8f71c61705daf7422b480cdf270babc8
hoangduyzodinet <duy.luong@zodinet.com>
2022-07-01 10:44:46 +0700

    init

---
+++ src/api/product-detail-page/routes/product-detail-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-detail-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product-detail-page.product-detail-page');
---
+++ src/api/product-detail-page/services/product-detail-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * product-detail-page service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::product-detail-page.product-detail-page');
